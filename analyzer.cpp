fa89e5f443d57186d46f0a7d5bb020124ac5eb55
Ishaan Desai <i.desai@ufl.edu>
2024-10-06 13:41:22 -0400

    new lab5 on clion

--- analyzer.cpp
+++ analyzer.cpp
@@ -30,6 +30,39 @@ int binary_search(const vector<string>& container, const string& element) {
 }
 
 int main() {
-    std::chrono::system_clock::now();
-    cout << "Time to find \"aaaaa\": ";
+    auto start = chrono::system_clock::now();
+    int result = linear_search(getStringData(), "aaaaa");
+    auto end = chrono::system_clock::now();
+    chrono::duration<double> timediff = end - start;
+    cout << "Time to find \"aaaaa\" using linear search: " << timediff.count() << ", index at " << result;
+
+    auto start2 = chrono::system_clock::now();
+    int result2 = binary_search(getStringData(), "aaaaa");
+    auto end2 = chrono::system_clock::now();
+    chrono::duration<double> timediff2 = end2 - start2;
+    cout << "Time to find \"aaaaa\" using binary search: " << timediff2.count() << ", index at " << result2;
+
+    auto start3 = chrono::system_clock::now();
+    int result3 = linear_search(getStringData(), "mzzzz");
+    auto end3 = chrono::system_clock::now();
+    chrono::duration<double> timediff3 = end3 - start3;
+    cout << "Time to find \"mzzzz\" using linear search: " << timediff3.count() << ", index at " << result3;
+
+    auto start4 = chrono::system_clock::now();
+    int result4 = binary_search(getStringData(), "mzzzz");
+    auto end4 = chrono::system_clock::now();
+    chrono::duration<double> timediff4 = end4 - start4;
+    cout << "Time to find \"mzzzz\" using binary search: " << timediff4.count() << ", index at " << result4;
+
+    auto start5 = chrono::system_clock::now();
+    int result5 = linear_search(getStringData(), "not_here");
+    auto end5 = chrono::system_clock::now();
+    chrono::duration<double> timediff5 = end5 - start5;
+    cout << "Time to find \"not_here\" using binary search: " << timediff5.count() << ", index at " << result5;
+
+    auto start6 = chrono::system_clock::now();
+    int result6 = binary_search(getStringData(), "not_here");
+    auto end6 = chrono::system_clock::now();
+    chrono::duration<double> timediff6 = end6 - start6;
+    cout << "Time to find \"not_here\" using binary search: " << timediff6.count() << ", index at " << result6;
 }
